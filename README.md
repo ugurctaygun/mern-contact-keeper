2268c8481458939e044bdf50cdc0d06dc3ad1983
ugurctaygun <ugurctaygun@gmail.com>
2020-03-07 18:40:29 +0300

    mongoose model, express validator, bcrypt

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # mern-contact-keeper
+Tutorial Project
 
 npm init -y
 
 npm i express bcryptjs jsonwebtoken config express-validator mongoose
 
 npm i -D nodemon concurrently
+
+npm run server
+
+MONGOOSE
+
+Create models for keeping the schemas of database, userSchema (user: type:string etc)
+Export models through route using require();
+
+EXPRESS
+
+Create a config file of db, introduce your database variables there and call your database to Server.js
+and call it as a method to initiate.
+
+Express-js-validator is used to limit the scope of the data that can be sent to the api
+
+BCRYPT
+
+is used to hash the password or objects that are required to be hashed while passing them to the database
+bcrypt.hash(param, method);
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check');
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/User');
 
 // @route POST api/users
 // @desc  Register a user
 // @acess Public
-router.post('/', (req,res) => {
-    res.send('Register a user');
+router.post('/', [
+    check('name', 'Please add Name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+], async(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {name,email,password} = req.body;
+
+    try {
+        let user = await User.findOne({email: email});
+        if(user) {
+            return res.status(400).json({ msg: 'User already exists'});
+        }
+        user = newUser({
+            name,
+            email,
+            password: password
+        });
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password,salt);
+
+        await user.save();
+
+        res.send('User saved');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
